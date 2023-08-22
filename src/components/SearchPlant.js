0dd7021d453f1ad4129fea9f7fa18a577c0b6a6f
gcot970 <gcot970@gmail.com>
2023-08-21 21:24:18 -0500

    Created empty components

---
+++ src/components/SearchPlant.js
@@ -0,0 +1,38 @@
+/*
+Please remove this in final!!
+                                                                   
+            ,,                                                 ,,  
+`7MM"""Mq.`7MM                       mm   `7MM"""Mq.         `7MM  
+  MM   `MM. MM                       MM     MM   `MM.          MM  
+  MM   ,M9  MM   ,6"Yb.  `7MMpMMMb.mmMMmm   MM   ,M9 ,6"Yb.    MM  
+  MMmmdM9   MM  8)   MM    MM    MM  MM     MMmmdM9 8)   MM    MM  
+  MM        MM   ,pm9MM    MM    MM  MM     MM       ,pm9MM    MM  
+  MM        MM  8M   MM    MM    MM  MM     MM      8M   MM    MM  
+.JMML.    .JMML.`Moo9^Yo..JMML  JMML.`Mbmo.JMML.    `Moo9^Yo..JMML.
+                                                                   
+                                                                   
+===================== Plant Search Page =====================
+                           Updated 
+                          08/21/2023
+-------------------------------------------------------------
+                         To-Do List
+-------------------------------------------------------------
+- Serve results via Pernual API via cards
+- Once clicked on card open modal
+- In modal, add quantity and nickname
+- Button in modal saves to database
+- Once saved, returns to UserPage
+
+*/
+
+// Import:
+import { Link } from 'react-router-dom';
+
+
+// Create page:
+const SearchPlant = () => {
+
+};
+
+// Export!
+export default SearchPlant;
