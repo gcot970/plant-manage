0f15d3b8f58b622ca71eb34b3dfbdc2d236182d3
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-08-27 17:46:42 -0500

    deletedfiles

--- routes/protected.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticate = require('../middleware/auth');
-
-router.get('/protected', authenticate, (req, res) => {
-  res.json({ message: 'You have access to this protected route' });
-});
-
-module.exports = router;
