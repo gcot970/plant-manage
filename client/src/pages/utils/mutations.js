cc46112f9c52e8670cb8e1ba4580d5eea1fb191e
gcot970 <gcot970@gmail.com>
2023-08-28 16:06:30 -0500

    Added mutations and queries on clientside for plants

--- client/src/pages/utils/mutations.js
+++ client/src/pages/utils/mutations.js
@@ -22,3 +22,46 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_PLANT = gql`
+  mutation addPlant(
+    $commonName: String!
+    $scientificName: String!
+    $nickname: String
+    $watering: String!
+  ) {
+    addPlant(
+      commonName: $commonName
+      scientificName: $scientificName
+      nickname: $nickname
+      watering: $watering
+    ) {
+      _id
+      commonName
+      scientificName
+      nickname
+      watering
+      addedDate
+      firstWaterDate
+      lastWaterDate
+    }
+  }
+`;
+
+export const DELETE_PLANT = gql`
+  mutation deletePlant($plantId: ID!) {
+    deletePlant(plantId: $plantId) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_PLANT = gql`
+  mutation updatePlant($plantId: ID!, $nickname: String, $lastWaterDate: Date) {
+    updatePlant(plantId: $plantId, nickname: $nickname, lastWaterDate: $lastWaterDate) {
+      _id
+      nickname
+      lastWaterDate
+    }
+  }
+`;
