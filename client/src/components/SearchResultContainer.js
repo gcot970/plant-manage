bfc5b46ba730e55e3d31c7fc2fc895cc0c62fb4c
Fernando Siller <fernando@Fernandos-MBP.attlocal.net>
2023-08-28 17:17:19 -0500

    more changes

--- client/src/components/SearchResultContainer.js
+++ client/src/components/SearchResultContainer.js
@@ -7,6 +7,8 @@ import search from '../utils/API';
 const SearchResultContainer = () => {
   // Declare a new state variable, "results"
   const [results, setResults] = useState([]);
+//   const [number, setNumber] = useState(0);
+    const [searchText, setSearchText] = useState('');
 
   // Method to get search results and set state
   const searchGiphy = async (query) => {
@@ -14,16 +16,22 @@ const SearchResultContainer = () => {
     setResults(response.data.data);
   };
 
-  // We want to run this method when the component first loads so that we have images of kittens to display
+
+
+  // We want to run this method when the component first loads so that we have images of plants to display
   // The second argument is the dependency array. This means that this method will only run when the component first loads
   useEffect(() => {
-    searchGiphy('kittens');
-  }, []);
+    searchGiphy(searchText);
+  }, [searchText]);
 
   return (
     <div>
       {/* Pass our results to the ResultsList component to map over */}
+      <input value={searchText} onChange={event => setSearchText(event.target.value)}></input>
       <ResultList results={results} />
+      {/* <h1>Number: {number}</h1>
+      <button onClick={() => setNumber(number + 1)}>Add</button>
+      <button onClick={() => setNumber(number + 1)}>Minus</button> */}
     </div>
   );
 };
